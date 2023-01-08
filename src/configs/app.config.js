74123a1819831f40891f755f42d3aec97f65fd8d
buidevkhanh <buiduckhanh.dev@gmail.com>
2023-01-08 15:48:13 +0700

    [KhanhBD] Change environment"

--- src/configs/app.config.js
+++ src/configs/app.config.js
@@ -1,5 +1,5 @@
-export const SERVER_HOST = "https://ojp-backend.onrender.com/";
-export const SOCKET_HOST = "https://ojp-backend.onrender.com/";
+export const SERVER_HOST = "https://ojp-backend.onrender.com";
+export const SOCKET_HOST = "https://ojp-backend.onrender.com";
 //export const SERVER_HOST = "https://17cb-118-70-236-74.ap.ngrok.io";
 export const SERVER_PREFIX = "/api/v1";
 export const POST = "post";
